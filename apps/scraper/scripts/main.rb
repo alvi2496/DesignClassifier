ea125b6451686b04cec79aceae92a1fb55a92422
alvi2496 <alvi.utsab@gmail.com>
2019-05-08 17:53:04 -0700

    complete parser

--- apps/scraper/scripts/main.rb
+++ apps/scraper/scripts/main.rb
@@ -3,6 +3,10 @@ load "scripts/scraper_and_saver.rb"
 puts "Enter Repository url: "
 repo_url = gets
 
+puts "Enter Pull Request number: "
+pr_number = gets
+
+pr_url = "#{repo_url.strip}/pull/#{pr_number.strip}"
 
 
-scraper_and_saver repo_url.strip
+scraper_and_saver pr_url
